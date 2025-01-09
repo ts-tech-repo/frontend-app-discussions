36b3b4ba1ac1c98c82221732803f76df62e1ecd2
dattatadepalli <datta.t@talentsprint.com>
2025-01-09 11:52:09 +0530

    Update api.js

--- src/discussions/data/api.js
+++ src/discussions/data/api.js
@@ -1,33 +1,21 @@
 /* eslint-disable import/prefer-default-export */
 
-import { ensureConfig, getConfig } from "@edx/frontend-platform";
-import { getAuthenticatedHttpClient } from "@edx/frontend-platform/auth";
+import { ensureConfig, getConfig } from '@edx/frontend-platform';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 
-ensureConfig(["LMS_BASE_URL"], "Posts API service");
+ensureConfig([
+  'LMS_BASE_URL',
+], 'Posts API service');
 
-export const getCourseConfigApiUrl = () =>
-  `${getConfig().LMS_BASE_URL}/api/discussion/v1/courses/`;
-export const getDiscussionsConfigUrl = (courseId) =>
-  `${getCourseConfigApiUrl()}${courseId}/`;
+export const getCourseConfigApiUrl = () => `${getConfig().LMS_BASE_URL}/api/discussion/v1/courses/`;
+export const getDiscussionsConfigUrl = (courseId) => `${getCourseConfigApiUrl()}${courseId}/`;
 /**
  * Get discussions course config
  * @param {string} courseId
  */
 export async function getDiscussionsConfig(courseId) {
-  try {
-    const { data } = await getAuthenticatedHttpClient().get(
-      getDiscussionsConfigUrl(courseId)
-    );
-    return data;
-  } catch (error) {
-    const { httpErrorStatus } = error && error.customAttributes;
-    if (httpErrorStatus === 404) {
-      global.location.replace(
-        `${getConfig().LMS_BASE_URL}/discussions/${courseId}/not-found`
-      );
-    }
-    throw error;
-  }
+  const { data } = await getAuthenticatedHttpClient().get(getDiscussionsConfigUrl(courseId));
+  return data;
 }
 
 /**
@@ -36,17 +24,6 @@ export async function getDiscussionsConfig(courseId) {
  */
 export async function getDiscussionsSettings(courseId) {
   const url = `${getDiscussionsConfigUrl(courseId)}settings`;
-
-  try {
-    const { data } = await getAuthenticatedHttpClient().get(url);
-    return data;
-  } catch (error) {
-    const { httpErrorStatus } = error && error.customAttributes;
-    if (httpErrorStatus === 404) {
-      global.location.replace(
-        `${getConfig().LMS_BASE_URL}/discussions/${courseId}/not-found`
-      );
-    }
-    throw error;
-  }
+  const { data } = await getAuthenticatedHttpClient().get(url);
+  return data;
 }
