aba73824d15e4a95594ecfc432cf00014955db59
dattatadepalli <datta.t@talentsprint.com>
2025-01-09 14:01:04 +0530

    Update api.js

--- src/data/api.js
+++ src/data/api.js
@@ -13,19 +13,7 @@ export async function getCourseBlocks(courseId, username) {
     block_types_filter: 'course,chapter,sequential,vertical,discussion',
     student_view_data: 'discussion',
   };
-
-  try {
-    const { data } = await getAuthenticatedHttpClient().get(getBlocksAPIURL(), {
-      params,
-    });
-    return data;
-  } catch (error) {
-    const { httpErrorStatus } = error && error.customAttributes;
-    if (httpErrorStatus === 404) {
-      global.location.replace(
-        `${getConfig().LMS_BASE_URL}/discussions/${courseId}/not-found`
-      );
-    }
-    throw error;
-  }
+  const { data } = await getAuthenticatedHttpClient()
+    .get(getBlocksAPIURL(), { params });
+  return data;
 }
